68b1b880f4680c20209211d5a261f6b605d98d2c
jujuzyt <zyt_soleila@126.com>
2019-10-06 20:40:52 -0400

    1006

--- app/ui.r
+++ app/ui.r
@@ -11,7 +11,7 @@ navbarPage("Airbnb Shiny App Project",
            tabPanel("page 1",
                     
                     div(class="outer",
-                        tags$style(".outer {position: fixed; top: 41px; left: 20px; right: 0; bottom: 0; overflow: hidden; padding: 0}"),
+                        tags$style(".outer {position: fixed; top: 41px; left: 20px; right: 0; bottom: 0; overflow: hidden; padding: 0; opacity: 0.92}"),
                         
                         
                         absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
@@ -32,31 +32,64 @@ navbarPage("Airbnb Shiny App Project",
                                       sliderInput("Price", "$/night", label = "What is your nightly price range?",
                                                   min = 0, max = 500, value = 100, step=1)
                                       
-                                      )
-                        
-                    
-                        
-                          
                         )
-
+                        
+                        
+                        
+                        
+                    )
                     
                     
-      
+                    
+                    
                     
                     
            ),
            
-           tabPanel("page 2"
+           tabPanel("page 2",
+                    leafletOutput("mymap2",height = 800, width = 'auto'),
                     
                     ####### INSERT CONTENT HERE ###########
-                    
-                    
-                    
-                    
-                    
+                    div(class="outer",
+                        tags$style(".outer {position: fixed; top: 41px; left: 20px; right: 0; bottom: 0; overflow: hidden; padding: 0}"),
+                        
+                        
+                        absolutePanel(id = "controls", class = "panel panel-default",
+                                      style="padding: 0 20px 20px 20px;",
+                                      style = "opacity: 0.75",
+                                      fixed = TRUE,
+                                      draggable = TRUE, top = 50, left = 0, right = 40, bottom = "auto",
+                                      width = 330, height = "auto", h3("Select:"),
+                                      selectInput("Neighbor", label = h4("Neighborhoods"), 
+                                                  choices = list("None Selected",
+                                                                 "All Neighborhoods", 
+                                                                 "Bronx", 
+                                                                 "Brooklyn",
+                                                                 "Manhattan", 
+                                                                 "Queens",
+                                                                 "Staten Island"), selected = "None Selected"),
+                                      
+                                      sliderInput("price_range", label = "Price:",
+                                                  min = 0, max = 1000, value = c(100, 400)),
+                                      selectInput("Room_Type", label = h4("Room Type"), 
+                                                  choices = list("None Selected",
+                                                                 "All Types", 
+                                                                 "Entire home/apt", 
+                                                                 "Private room",
+                                                                 "Shared room"
+                                                  ), selected = "None Selected")
+                                      
+                                      
+                                      
+                                      
+                        )
+                        
+                        
+                        
+                    )
                     
                     #######################################
-                    )
+           )
            
            
            
